79c3c98050b459aacb3f53f5721c4d5d0274deca
Pau Mezzadra <paumezzadra@hotmail.com>
2021-03-29 10:24:23 -0300

    Add project files.

---
+++ ValoresNumericosEjercicio15/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ValoresNumericosEjercicio15
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int valor = 0;
+
+            Console.WriteLine("Ingrese un valor numérico");
+            if (!int.TryParse(Console.ReadLine(), out valor))
+            {
+                Console.WriteLine("Debe ser un valor numérico");
+            }
+            else
+            {
+                Console.WriteLine($"El valor ingresado es: {valor}");
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
